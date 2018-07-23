7dcc5306641ff986130d4b43d2fd55d290321513
IOLevi <337@holbertonschool.com>
2018-07-23 14:34:14 -0700

    working through problem 6

--- models/state.py
+++ models/state.py
@@ -3,11 +3,18 @@
     Implementation of the State class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
 class State(BaseModel):
     '''
         Implementation for the State.
     '''
-    name = ""
+    name = Column(String(128), nullable=False)
+    __table_name__ = "states"
+
+    #come back and add DBStorage and FileStorage 
+    #things
+    
+
