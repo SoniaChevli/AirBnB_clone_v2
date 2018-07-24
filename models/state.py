99ab2a0a6183a630d42e1b9af60d6d9613784770
IOLevi <337@holbertonschool.com>
2018-07-24 11:58:56 -0700

    changes to state from 6

--- models/state.py
+++ models/state.py
@@ -5,16 +5,26 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+from models import storage
+from models import City
 
 
 class State(BaseModel):
     '''
         Implementation for the State.
     '''
-    name = Column(String(128), nullable=False)
     __table_name__ = "states"
+    name = Column(String(128), nullable=False)
 
-    #come back and add DBStorage and FileStorage 
-    #things
-    
-
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        cities = relationship("City", cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            a = []
+            for i in storage.all(City):
+                if i.state_id == self.id:
+                    a.append(i)
+            return a
