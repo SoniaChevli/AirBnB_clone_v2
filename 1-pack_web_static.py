f255407cbca94f9c0330bcbf842807a197eec5fa
Sonia Chevli <soniachevli@gmail.com>
2018-07-26 18:05:10 +0000

    first commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+Write a Fabric script that
+generates a .tgz archive
+from the contents of the web_static
+'''
+from fabric.api import local
+import datetime
+
+
+def do_pack():
+    ''' generates a .tgz archive '''
+    try:
+        now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        local('sudo mkdir -p ./versions')
+        local("sudo tar -cvzf versions/web_static_{}.tgz web_static/"
+              .format(now))
+        return "versions/web_static_{}.tgz".format(now)
+    except:
+        return None
