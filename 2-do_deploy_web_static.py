2b206e85fa011d6c26757fd1e593ff16b9f1dffc
Sonia Chevli <soniachevli@gmail.com>
2018-08-16 16:03:43 +0000

    add try adn except statemnets

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,18 +33,15 @@ def do_deploy(archive_path):
     archivefile = filename.split('.')[0]
     archivefolder = ("/data/web_static/releases/{}".format(archivefile))
 
-    put(archive_path, "/tmp/{}".format(filename))
-    run("mkdir -p {}".format(archivefolder))
-    run("tar -xzf /tmp/{} -C {}".format(filename, archivefolder))
-    run('rm /tmp/{}'.format(filename))
-    run('mv {}/web_static/* {}'.format(archivefolder, archivefolder))
-    run('rm -rf {}/web_static/'.format(archivefolder))
-    run('rm -rf /data/web_static/current')
-    run('ln -s {} /data/web_static/current'.format(archivefolder))
-    print('works')
-    return True
-'''
+    try:
+        put(archive_path, "/tmp/{}".format(filename))
+        run("mkdir -p {}".format(archivefolder))
+        run("tar -xzf /tmp/{} -C {}".format(filename, archivefolder))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}/web_static/* {}'.format(archivefolder, archivefolder))
+        run('rm -rf {}/web_static/'.format(archivefolder))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(archivefolder))
+        return True
     except:
-        print('fail')
         return False
-'''
