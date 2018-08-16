03e576fe3539a0a8d595d16bcedd03fe213c42e9
Sonia Chevli <soniachevli@gmail.com>
2018-08-16 16:42:25 +0000

    remove 500 error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,14 +4,17 @@ listens on 0.0.0.0 port 5000
 '''
 from models import storage
 from flask import Flask, render_template
+import operator
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     ''' /states_list: adds a list of states to html '''
-    states = storage.all('State')
-    state_list = [i for value in states.values()]
+    states = storage.all("State")
+    state_list = list(states.values())
+
     return render_template('7-states_list.html', states=state_list)
 
 
