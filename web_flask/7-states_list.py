a023b37f1cd90304ee9bb65cbbcd9641bc8d881e
Sonia Chevli <soniachevli@gmail.com>
2018-08-16 01:30:24 +0000

    first commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+listens on 0.0.0.0 port 5000
+'''
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    ''' /states_list: adds a list of states to html '''
+    states = storage.all('State')
+    state_list = [i for value in states.values()]
+    return render_template('7-states_list.html', states=state_list)
+
+
+@app.teardown_appcontext
+def teardown(value):
+    ''' remove the current SQLAlchemy Session '''
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
