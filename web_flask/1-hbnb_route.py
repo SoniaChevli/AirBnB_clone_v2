8189391b93c44708762b53f2b5223b742ec9e89b
Sonia Chevli <soniachevli@gmail.com>
2018-08-22 12:49:11 +0000

    remove ! after HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,7 +17,7 @@ def hello_HBNB():
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
-    return 'HBNB!'
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
